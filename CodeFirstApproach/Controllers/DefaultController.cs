a7c2d32e4b963b26b4dd26ca0190ad7604983a4a
firoz.ali <firoz.ali@K-FIROZNP>
2019-10-26 21:14:50 +0300

    Add project files.

---
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstApproach.Models;
+using System.Data.Entity;
+namespace CodeFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            List<EmployeeModel> employeedata = db.EmployeeModels.ToList();            
+            return View(employeedata);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection frm)
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpName = frm["EmpName"];
+            obj.EmpSalary = Convert.ToInt32(frm["EmpSalary"]);
+            obj.DeptId = Convert.ToInt32(frm["DeptId"]);
+
+            db.EmployeeModels.Add(obj);
+           int i= db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel obj = db.EmployeeModels.Find(id);
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel obj)
+        {
+            db.Entry(obj).State = EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel obj = db.EmployeeModels.Find(id);
+            return View(obj);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? Empid)
+        {
+            EmployeeModel obj = db.EmployeeModels.Find(Empid);
+            db.EmployeeModels.Remove(obj);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CodeFirstApproach/Models/EmployeeModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstApproach.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+
+        public int EmpSalary { get; set; }
+        public int DeptId { get; set; }
+    }
+
+    public class DepartmentModel
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    }
+
+    public class Empdept
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+        public string DeptName { get; set; }
+
+    }
+}
